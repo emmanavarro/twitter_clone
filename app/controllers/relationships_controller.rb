a2cfbd2c2a92c2bd550c3be080d9a9b03d0a7a5b
Emma Navarro <elnavarro55@gmail.com>
2021-03-24 09:44:54 -0500

    Redirect to user profile

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to root_path
+    redirect_to user_path(user), notice: "Now you are following @#{user.username}"
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to root_path
+    redirect_to user_path(user), notice: "You are not following @#{user.username}"
   end
 end
